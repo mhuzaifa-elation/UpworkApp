c1a6b3c25cced1ddbe5346226737a7f9f6ec940e
Huzaifa Awan <mhuzaifa@elationerp.com>
2022-06-24 21:35:27 +0500

    Initial commit

---
+++ UpworkApp/UpworkApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UpworkApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new ConceptTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UpworkApp/UpworkApp/SampleTabbedPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UpworkApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SampleTabbedPage : ContentPage
+    {
+        private List<string> _sampleText;
+
+        public List<string> SampleText
+        {
+            get => _sampleText;
+            set
+            {
+                if (value == _sampleText)
+                {
+                    return;
+                }
+                _sampleText = value;
+                OnPropertyChanged(nameof(SampleText));
+            }
+        }
+        public SampleTabbedPage()
+        {
+        
+            InitializeComponent();
+            BindingContext = this;
+            SampleText= new List<string>()  {
+                        "New York",
+                        "London",
+                        "Mumbai",
+                        "Chicago"
+                    };
+        }
+    }
+}
